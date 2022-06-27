470223d18af985b4cea3429963f32c92b442e62a
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-06-27 14:35:10 -0500

    rendering current todo list now

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,18 +1,49 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
+<script >
+
+export default {
+    name: 'todo-list',
+    data () {
+        return {
+            newTodo: '', 
+            todos: [
+                {
+                    'id': 1,
+                    'title': 'Finish Vue Screencast',
+                    'completed': false,
+                },
+                {
+                    'id': 2,
+                    'title': 'Take over world',
+                    'completed': false,
+                }
+            ]
+
+        }
+    }
+} 
+
 </script>
 
 <template>
   <div >
-  Todo List goes here
+  <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo">
+  <div v-for="todo in todos" :key="todo.id" class="todo-item">
+{{ todo.title }}
+  </div>
   </div>
 </template>
 
-<style>
 
+
+<style >
+.todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+
+    &:focus {
+        outline: 0;
+    }
+}
 </style>
